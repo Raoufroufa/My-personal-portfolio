8a3a4cce302bfd23da8e4d5508be830d2886ab60
Raouf <97788391+Raoufroufa@users.noreply.github.com>
2022-11-20 13:25:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("contact-page").onclick = function () {
+	alert("Hello! Welcome to my contact page :)");
+};
+
+document.getElementById("home-p").onclick = function () {
+	alert("Hello! Welcome to my home page :)");
+};
+
+document.getElementById("me").onclick = function () {
+	alert("Hello! Welcome to my resume part :)");
+};
+
+document.getElementById("proj").onclick = function () {
+	alert("Hello! Welcome to my relized projects :)");
+};
+
+document.getElementById("skill").onclick = function () {
+	alert("Hello! Welcome to my skills :)");
+};
